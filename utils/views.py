7d8407df21cc2e8e344a7504e0d7e8febc527208
Nima Esmaeili <nimaww13801030@gmail.com>
2022-06-13 20:45:39 +0430

    fix bug

--- utils/views.py
+++ utils/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
-    max_page_size = 10000
+    max_page_size = 1000
     page_size = 5
 
     def get_paginated_response(self, data):
